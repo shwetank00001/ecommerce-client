4e53f9729ad1519148ababb2d612e2c9943d4fd1
shwetank00001 <mshwetank@gmail.com>
2023-10-24 15:04:45 +0530

    cart done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage'
+import Cart from './features/cart/Cart'
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/cart" element={<Cart />} />
       </Routes>
   );
 }
--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -3,6 +3,8 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
+import { Link } from 'react-router-dom';
+
 
 const user = {
     name: "Tom Cook",
@@ -65,19 +67,21 @@ const Navbar = ({children}) => {
                                   </div>
                                   <div className="hidden md:block">
                                       <div className="ml-4 flex items-center md:ml-6">
-                                          <button
-                                              type="button"
-                                              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                          >
-                                              <span className="absolute -inset-1.5" />
-                                              <ShoppingCartIcon
-                                                  className="h-6 w-6"
-                                                  aria-hidden="true"
-                                              />
-                                              <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
-                                                  3
-                                              </span>
-                                          </button>
+                                          <Link to="/cart">
+                                              <button
+                                                  type="button"
+                                                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                              >
+                                                  <span className="absolute -inset-1.5" />
+                                                  <ShoppingCartIcon
+                                                      className="h-6 w-6"
+                                                      aria-hidden="true"
+                                                  />
+                                                  <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+                                                      3
+                                                  </span>
+                                              </button>
+                                          </Link>
 
                                           {/* Profile dropdown */}
                                           <Menu
@@ -202,22 +206,24 @@ const Navbar = ({children}) => {
                                               {user.email}
                                           </div>
                                       </div>
-                                      <button
-                                          type="button"
-                                          className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                      >
-                                          <span className="absolute -inset-1.5" />
-                                          <span className="sr-only">
-                                              View notifications
-                                          </span>
-                                          <ShoppingCartIcon
-                                              className="h-6 w-6"
-                                              aria-hidden="true"
-                                          />
-                                          <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
-                                              3
-                                          </span>
-                                      </button>
+                                      <Link to='/cart'>
+                                          <button
+                                              type="button"
+                                              className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                          >
+                                              <span className="absolute -inset-1.5" />
+                                              <span className="sr-only">
+                                                  View notifications
+                                              </span>
+                                              <ShoppingCartIcon
+                                                  className="h-6 w-6"
+                                                  aria-hidden="true"
+                                              />
+                                              <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+                                                  3
+                                              </span>
+                                          </button>
+                                      </Link>
                                   </div>
                                   <div className="mt-3 space-y-1 px-2">
                                       {userNavigation.map((item) => (
